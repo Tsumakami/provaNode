0315acdaf438e17712fef13b398309719e8520c7
João Paulo Malvesti <jpmf1995@gmail.com>
2019-03-25 01:52:04 -0300

    Adicionado estilização e os links para edição e remoção

--- src/app/views/medicos/lista/lista.marko.js
+++ src/app/views/medicos/lista/lista.marko.js
@@ -7,27 +7,46 @@ var marko_template = module.exports = require("marko/src/html").t(__filename),
     marko_renderer = components_helpers.r,
     marko_defineComponent = components_helpers.c,
     marko_helpers = require("marko/src/runtime/html/helpers"),
+    marko_loadTag = marko_helpers.t,
+    component_globals_tag = marko_loadTag(require("marko/src/components/taglib/component-globals-tag")),
     marko_forEach = marko_helpers.f,
-    marko_escapeXml = marko_helpers.x;
+    marko_escapeXml = marko_helpers.x,
+    marko_attr = marko_helpers.a,
+    init_components_tag = marko_loadTag(require("marko/src/components/taglib/init-components-tag")),
+    await_reorderer_tag = marko_loadTag(require("marko/src/taglibs/core/await/reorderer-renderer"));
 
 function render(input, out, __component, component, state) {
   var data = input;
 
-  out.w("<table class=\"table\"><thead><tr><th scope=\"col\">Nome</th><th scope=\"col\">Especialidade</th></tr></thead><tbody>");
+  out.w("<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"/estatico/css/bootstrap.css\"></head><body>");
 
-  var for__6 = 0;
+  component_globals_tag({}, out);
+
+  out.w("<h1>Lista de Médicos</h1><table class=\"table\" id=\"medico\"><thead><tr><th scope=\"col\">Nome</th><th scope=\"col\">Especialidade</th></tr></thead><tbody>");
+
+  var for__11 = 0;
 
   marko_forEach(data.medicos, function(medico) {
-    var keyscope__7 = "[" + ((for__6++) + "]");
+    var keyscope__12 = "[" + ((for__11++) + "]");
 
-    out.w("<tr><th scope=\"row\">" +
+    out.w("<tr" +
+      marko_attr("id", "medico_" + medico.id) +
+      "><th scope=\"row\">" +
       marko_escapeXml(medico.nome) +
       "</th><td>" +
       marko_escapeXml(medico.especialidade) +
-      "</td></tr>");
+      "</td><td><a href=\"#\">Editar</a></td><td><a href=\"#\"" +
+      marko_attr("data-ref", "" + medico.id) +
+      " data-type=\"remocao\">Excluir</a></td></tr>");
   });
 
-  out.w("</tbody></table>");
+  out.w("</tbody></table><script src=\"/estatico/js/remove-medico.js\">\n</script>");
+
+  init_components_tag({}, out);
+
+  await_reorderer_tag({}, out, __component, "21");
+
+  out.w("</body></html>");
 }
 
 marko_template._ = marko_renderer(render, {
@@ -38,5 +57,10 @@ marko_template._ = marko_renderer(render, {
 marko_template.Component = marko_defineComponent({}, marko_template._);
 
 marko_template.meta = {
-    id: "/provanodejs$1.0.0/src/app/views/medicos/lista/lista.marko"
+    id: "/provanodejs$1.0.0/src/app/views/medicos/lista/lista.marko",
+    tags: [
+      "marko/src/components/taglib/component-globals-tag",
+      "marko/src/components/taglib/init-components-tag",
+      "marko/src/taglibs/core/await/reorderer-renderer"
+    ]
   };
